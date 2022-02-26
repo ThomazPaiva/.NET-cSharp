8fe5684151283babf606dc0ce8a05beea7001b75
Thomaz Paiva <69321375+ThomazPaiva@users.noreply.github.com>
2022-02-26 12:06:25 -0300

    C# Console Application

---
+++ guessNumber/ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number inbetween 0 and 100: ");
+                int n = int.Parse(Console.ReadLine());
+
+                if(n > winNum)
+                {
+                    Console.WriteLine("To high ! Guess lower");
+                }
+
+                else if (n < winNum)
+                {
+                    Console.WriteLine("To low");
+                }
+                
+                else if (n == winNum)
+                {
+                    Console.WriteLine("You win !");
+                    win = true;
+                }
+                Console.WriteLine();
+
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing the game");
+            Console.Write("Press any key to finish");
+            Console.ReadKey(true);
+        }
+
+    }
+}
